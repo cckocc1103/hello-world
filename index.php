bad4969dae30c87285088f56e6bd255bb6cf633a
cckocc1103 <cckocc@yahoo.co.jp>
2018-11-25 22:28:42 +0900

    q

--- index.php
+++ index.php
@@ -6,13 +6,13 @@ require_once __DIR__ . '/vendor/autoload.php';
 $inputString = file_get_contents('php://input');
 error_log($inputString)
 */
-
+error_log("0000");
 // アクセストークンを使いCurlHTTPClientをインスタンス化
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-
+error_log("1111");
 // CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
-
+error_log("2222");
 // LINE Messaging APIがリクエストに付与した署名を取得
 $signature = $_SERVER['HTTP' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
