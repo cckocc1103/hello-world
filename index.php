9777924e12b45dcd8643460baae9447d4cf9b1c2
cckocc1103 <cckocc@yahoo.co.jp>
2018-11-25 22:34:22 +0900

    a

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ error_log("1111");
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
 error_log("2222");
 // LINE Messaging APIがリクエストに付与した署名を取得
-$signature = $_SERVER['HTTP' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
 // 署名が正当かチェック。正当であればリクエストをパースし配列へ
 $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
